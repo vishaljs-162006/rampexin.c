8210e64b6670f8a9ded5a03f5e69508c9a06c42d
vishaljs-162006 <vishaljs162006@gmail.com>
2025-05-17 10:28:57 +0530

    Add files via upload

---
+++ day 5/sum.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int num, sum = 0, digit;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    // Make sure to handle negative numbers
+    if (num < 0) num = -num;
+
+    while (num > 0) {
+        digit = num % 10;
+        sum += digit;
+        num /= 10;
+    }
+
+    printf("Sum of digits: %d\n", sum);
+
+    return 0;
+}
